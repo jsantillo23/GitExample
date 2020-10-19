4475e5ef5286bd21caef94b410e646f4697e00d9
joe <joe@10.0.0.138>
2020-10-19 17:27:07 -0400

    Added getters and setters

--- GitExample/src/ItemToPurchase.java
+++ GitExample/src/ItemToPurchase.java
@@ -3,6 +3,36 @@ public class ItemToPurchase {
     
     
     String itemName;
+    public String getItemName() {
+        return itemName;
+    }
+
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+
     int itemPrice;
     int itemQuantity;
     
